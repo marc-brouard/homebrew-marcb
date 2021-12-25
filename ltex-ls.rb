d0d73f459110a58686ef92e2d3ab507206f2b1e6
Marc Brouard <brouard.marc@gmail.com>
2021-12-25 18:40:15 +0000

    Version 0.1.0

--- ltex-ls.rb
+++ ltex-ls.rb
@@ -14,7 +14,7 @@ class LtexLs < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system './configure', *std_configure_args, '--disable-silent-rules'
+    # system './configure', *std_configure_args, '--disable-silent-rules'
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     bin.install 'bin/ltex-cli'
     bin.install 'bin/ltex.ls'
